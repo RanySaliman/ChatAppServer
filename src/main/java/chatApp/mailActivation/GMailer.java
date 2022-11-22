2fedeea20d896cc22facd8e66a2924076e314021
Yael Adler <yaeli.adler@gmail.com>
2022-11-22 10:03:16 +0200

    GMailer.java was added

--- src/main/java/chatApp/mailActivation/GMailer.java
+++ src/main/java/chatApp/mailActivation/GMailer.java
@@ -1,4 +1,15 @@
 package chatApp.mailActivation;
 
 public class GMailer {
+    private void sendMail(String chatApp, String message) {
+        //send email to ourselves
+
+
+    }
+
+    public static void main(String[] args) {
+        new GMailer().sendMail("ChatApp", "this is activation token please click it in order to login to the chatApp application");
+    }
+
+
 }
