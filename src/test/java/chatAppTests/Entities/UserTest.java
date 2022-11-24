25f1f64237f5ff225b56345ffe3cd7ec53ebc223
Rany Saliman <ranysaliman@gmail.com>
2022-11-24 10:39:26 +0200

    adding tesets

---
+++ src/test/java/chatAppTests/Entities/UserTest.java
@@ -0,0 +1,68 @@
+package chatAppTests.Entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void setFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void setLastName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void isEnabled() {
+    }
+
+    @Test
+    void setEnabled() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
